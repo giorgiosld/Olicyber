ddbb88b9c960a7e4aa4e7b2805f342fb5b356e80
girogiosld <giorgiosaldana99@gmail.com>
2023-04-30 20:49:30 +0200

    Crypto5

--- crypto/crypto5.py
+++ crypto/crypto5.py
@@ -8,7 +8,7 @@ for o in range(256):
     plaintext = xor(bytes.fromhex(ciphertext), o.to_bytes(1, "big")*25)
     if plaintext.isascii():
         print(str(hex(o)))
-        print('flag{' + str(plaintext) + '}')
+        print(str(plaintext))
 
 #found that the key is strgit "0x20"*25 so "20"*25
 print('flag{' + str(xor(bytes.fromhex(ciphertext), bytes.fromhex("20"*25))) + '}')
