0cb6753630db2fd1918c73ef2c67b37a01cb5016
girogiosld <giorgiosaldana99@gmail.com>
2023-05-01 19:36:10 +0200

    no_robot_here.py

--- web/no_robots_here.py
+++ web/no_robots_here.py
@@ -1,1 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
+
+URL = "http://no-robots.challs.olicyber.it/"
+
+r = requests.get(URL)
+print(r.text)
+
+ROBOTS = URL+"robots.txt"
+r = requests.get(ROBOTS)
+print(str(r.text)[24:])
+FLAG_URI = str(r.text)[24:]
+
+FLAG = URL+FLAG_URI
+r = requests.get(FLAG)
+soup = BeautifulSoup(r.text, 'html.parser')
+flag = soup.find_all('h2')
+print(str(flag)[5:-5])
